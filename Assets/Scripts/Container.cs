fb0076a2596b9ca5c442bf95a9beb2b57bb1ac0f
Onur Öztop <onureldrinx@gmail.com>
2024-01-26 02:08:38 +0300

    drink traversal logic base

--- Assets/Scripts/Container.cs
+++ Assets/Scripts/Container.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class Container : MonoBehaviour
@@ -8,18 +9,63 @@ public class Container : MonoBehaviour
     [SerializeField] private List<Transform> drinkSlots;
     [SerializeField] private List<Drink> drinks;
     private BoxCollider boxCollider;
+    private HashSet<DrinkType> existingTypes;
+    private Dictionary<DrinkType, int> typeCountMap;
+
     private void Start()
     {
         if(grid is not null) drinkSlots.AddRange(grid.GetCells());
 
         boxCollider = GetComponent<BoxCollider>();
 
+        existingTypes = new HashSet<DrinkType>();
+        
+        foreach (var t in drinkSlots.Where(t => t.childCount > 0))
+        {
+            Drink temp = t.GetChild(0).GetComponent<Drink>();
+            drinks.Add(temp);
+            existingTypes.Add(temp.GetDrinkType());
+        }
+
+    }
+
+    public List<List<Drink>> CategorizeDrinks()
+    {
+        typeCountMap = new Dictionary<DrinkType, int>();
+
+        List<List<Drink>> drinkGroups = new List<List<Drink>>();
+
+        for (int i = 0; i < existingTypes.Count; i++)
+        {
+            List<Drink> temp = new List<Drink>();
+            for (int j = 0; j < drinks.Count; j++)
+            {
+                if (drinks[j].GetDrinkType() == existingTypes.ElementAt(i))
+                {
+                    temp.Add(drinks[j]);
+                }
+            }
+            drinkGroups.Add(temp);
+        }
+
+        //drinkGroups.Sort((x, y) => x.Count.CompareTo(y.Count));
+        drinkGroups.Sort(SortByAscendingListCount);
+
+        foreach (var group in drinkGroups)
+        {
+            typeCountMap.Add(group[0].GetDrinkType(),group.Count);
+            print(group[0].GetDrinkType()+"-"+group.Count);
+        }
+
+        return drinkGroups;
+
     }
     
     
-    
-    
-    
+    private int SortByAscendingListCount(List<Drink> x, List<Drink> y)
+    {
+        return x.Count.CompareTo(y.Count);
+    }
     
 
     public void Fill(List<Drink> drinksList)
--- Assets/Scripts/ContainerSlot.cs
+++ Assets/Scripts/ContainerSlot.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class ContainerSlot : MonoBehaviour
@@ -35,10 +36,43 @@ public class ContainerSlot : MonoBehaviour
             
             List<Transform> neighbors = grid.GetNeighborsOf(cell);
 
+            Dictionary<Container, List<List<Drink>>> containerDrinkGroupMap = new Dictionary<Container, List<List<Drink>>>();
+
             foreach (var n in neighbors)
             {
                n.gameObject.SetActive(false);
+
+               Container tempContainer = n.GetComponent<GridCell>().content.GetComponent<ContainerSlot>().currentContainer;
+
+               if (tempContainer is not null)
+               {
+                   List<List<Drink>> tempGroupList = tempContainer.CategorizeDrinks();
+                   containerDrinkGroupMap.Add(tempContainer,tempGroupList);
+
+               }
+               
             }
+            
+            List<List<Drink>> thisGroupList = currentContainer.CategorizeDrinks();
+            
+            containerDrinkGroupMap.Add(currentContainer,thisGroupList);
+
+            
+            
+            /*for (int i = 0; i < containerDrinkGroupMap.Keys.Count; i++)
+            {
+                Container c = containerDrinkGroupMap.Keys.ElementAt(i);
+                List<Drink> l = containerDrinkGroupMap[c];
+
+                var containers = containerDrinkGroupMap.Where(query => query.Value.Count > l.Count && query.Value[0].GetDrinkType() == l[0].GetDrinkType());
+
+                foreach (var containerCurrent in containers)
+                {
+                    print(containerCurrent.Key.transform.parent.parent.name);
+                }
+                
+            }*/
+            
 
         }
         else
@@ -47,8 +81,11 @@ public class ContainerSlot : MonoBehaviour
         }
         
     }
-    
-    
+
+    public Container GetCurrentContainer()
+    {
+        return currentContainer;
+    }
     
     
     
